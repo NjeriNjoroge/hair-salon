e72f2672fd73eae0ff46163cb9799ee01e2c0453
NjeriNjoroge <wanjorogegrace@gmail.com>
2017-08-24 18:29:23 +0300

    added the routes for stylist homepage, listing stylists, displaying a specific stylist and the form for creating a stylist

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Map;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
@@ -8,5 +9,35 @@ public class App {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
+//displays the index page when Spark is launched
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+//lists all Stylists
+      get("/stylists", (request, response) -> {
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("stylists", Stylist.all());
+        model.put("template", "templates/stylists.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
+
+//displays Stylists details
+        get("/stylists/:id", (request, response) -> {
+          Map<String, Object> model = new HashMap<String, Object>();
+          Stylist stylist = Stylist.find(Integer.parseInt(request.params(":id")));
+          model.put("stylist", stylist);
+          model.put("template", "templates/stylist.vtl");
+          return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+//displays the Stylist form
+      get("/stylists/new", (request, response) -> {
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("template", "templates/stylist-form.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
   }
 }
